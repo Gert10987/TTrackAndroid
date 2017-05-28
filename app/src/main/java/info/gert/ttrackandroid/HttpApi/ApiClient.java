9546460fc20650428da48585a1b102508e6b2673
gert <pawel.kalasz@gmail.com>
2017-05-28 12:50:47 +0200

    Added Employer Activity

--- app/src/main/java/info/gert/ttrackandroid/HttpApi/ApiClient.java
+++ app/src/main/java/info/gert/ttrackandroid/HttpApi/ApiClient.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     public static Retrofit retrofit = null;
-    private static String baseUrl = "https://test-ttrack.herokuapp.com/restApi/";
+    private static String baseUrl = "url_service/";
 
     public static Retrofit login(final String username, final String password) {
 
